9007467a8d57351d8e7ffb97cfba39bf63e163a4
Louis John Iaeger <louis.iaeger@gmail.com>
2026-01-15 19:57:41 -0600

    Fix login form issues

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -12,7 +12,6 @@ export function LoginForm({
   return (
     <form
       action={signInAction}
-      method="post"
       className={cn("flex flex-col gap-6", className)}
       {...props}
       suppressHydrationWarning
